457d2e81631cd3d01d03a4a15702d5c819c48f3a
Artem <artem.antonov1993@gmail.com>
2020-04-05 00:03:49 +0300

    feat: add movement of blocks, the ability to resize, timer

--- script.js
+++ script.js
@@ -114,3 +114,233 @@ function createField(field) {
 }
 
 createField(fields['gameBoard4*4']);
+
+
+// Moving Cubs
+const container = document.querySelector('.container');
+container.addEventListener('click', (event) => {
+  const freeElement = container.querySelector('.free');
+  const movingCub = event.target;
+  let coordinateFree = freeElement.getBoundingClientRect();
+  let coordinateMovingCub = movingCub.getBoundingClientRect();
+
+  if (movingCub.classList.contains('cub') && !movingCub.classList.contains('free')) {
+    if (coordinateMovingCub.left + coordinateMovingCub.width === coordinateFree.left && coordinateMovingCub.top === coordinateFree.top) {
+      movingCubs();
+      countSteps();
+    }
+
+    if (coordinateMovingCub.top + coordinateMovingCub.height === coordinateFree.top && coordinateMovingCub.left === coordinateFree.left) {
+      movingCubs();
+      countSteps();
+    }
+
+    if (coordinateMovingCub.top - coordinateMovingCub.height === coordinateFree.top && coordinateMovingCub.left === coordinateFree.left) {
+      movingCubs();
+      countSteps();
+    }
+
+    if (coordinateMovingCub.left - coordinateMovingCub.width === coordinateFree.left && coordinateMovingCub.top === coordinateFree.top) {
+      movingCubs();
+      countSteps();
+    }
+  }
+
+  function movingCubs() {
+    const outerMovingCub = movingCub.outerHTML;
+    movingCub.outerHTML = freeElement.outerHTML;
+    freeElement.outerHTML = outerMovingCub;
+  }
+});
+
+// const cubs = document.querySelectorAll('.cub');
+// console.log(cubs);
+// cubs.forEach((item, index) => {
+//   item.addEventListener('mousedown', (event) => {
+//     const freeClone = container.querySelector('.free').cloneNode(false).outerHTML;
+//     const firstIndex = cubs[index];
+//     cubs[index].outerHTML = freeClone;
+//     item.style.position = 'absolute';
+//     item.style.zIndex = 5;
+//     document.body.append(item);
+
+//     console.log(item.getAttribute('left'));
+//     console.log(item.getBoundingClientRect());
+
+//     moveAt(event.pageX, event.pageY);
+
+//     function moveAt(pageX, pageY) {
+//       item.style.left = pageX - item.offsetWidth / 2 + 'px';
+//       item.style.top = pageY - item.offsetHeight / 2 + 'px';
+//     }
+
+//     function onMouseMove(event) {
+//       moveAt(event.pageX, event.pageY);
+//     }
+
+//     document.addEventListener('mousemove', onMouseMove);
+
+
+//     item.onmouseup = function () {
+
+//       container.append(item);
+//       item.style.position = 'static';
+//       document.querySelectorAll('.cub')[index].outerHTML = item.outerHTML;
+//       item.remove;
+//       document.removeEventListener('mousemove', onMouseMove);
+//       item.onmouseup = null;
+//     };
+//   });
+// });
+
+
+// moveAt(event.pageX, event.pageY);
+
+// 	function moveAt(pageX, pageY) {
+// 		item.style.left = pageX - item.offsetWidth / 2 + 'px';
+// 		item.style.top = pageY - item.offsetHeight / 2 + 'px';
+// 	}
+
+// 	function onMouseMove(event) {
+// 		moveAt(event.pageX, event.pageY);
+
+// 	}
+
+// 	document.addEventListener('mousemove', onMouseMove);
+
+// 	item.onmouseup = function(event) {
+// 		container.append(item);
+// 		item.style.zIndex = 2;
+
+// 		document.removeEventListener('mousemove', onMouseMove);
+// 		container.onmouseup = null;
+
+// 		if (event.pageX >= coordinateFree.x && event.pageX <= coordinateFree.x + coordinateFree.width && event.pageY >= coordinateFree.y && event.pageY <= coordinateFree.y + coordinateFree.width &&
+// 			(leftItem + item.getBoundingClientRect().width  === leftFreeElement && topItem === topFreeElement ||
+// 			 topItem + item.getBoundingClientRect().height === topFreeElement && leftItem === leftFreeElement ||
+// 			 leftItem - item.getBoundingClientRect().width  === leftFreeElement && topItem === topFreeElement ||
+// 			 topItem - item.getBoundingClientRect().height === topFreeElement && leftItem === leftFreeElement)) {
+// 			console.log('hi');
+// 			item.style.top = topFreeElement + 'px';
+// 			item.style.left = leftFreeElement + 'px';
+// 			freeElement.style.top = topItem + 'px';
+// 			freeElement.style.left = leftItem + 'px';
+// 		} else {
+// 			item.style.top = topItem + 'px';
+// 		    item.style.left = leftItem + 'px';
+// 		}
+
+
+// 	};
+
+// })
+
+// })
+
+
+
+
+
+// Count Steps
+const countSteps = (function () {
+  let count = 0;
+  return function () {
+    count += 1;
+    document.querySelector('.steps-current').innerHTML = count;
+  };
+}());
+
+
+const optionField = document.querySelector('.option-field');
+
+// Timer
+const timer = function () {
+  let countMinutes = 0;
+  let countSeconds = 0;
+  const timerId = setInterval(() => {
+    if (document.querySelector('.stop').classList.contains('stop-timer')) {
+      console.log('fr');
+      clearInterval(timerId);
+    }
+    countSeconds += 1;
+    if (countSeconds > 59) {
+      countSeconds = 0;
+      countMinutes += 1;
+      document.querySelector('.minutes').innerHTML = countMinutes;
+    }
+
+    document.querySelector('.seconds').innerHTML = countSeconds;
+  }, 1000);
+  return timerId;
+};
+
+// Event with click on option buttons
+optionField.addEventListener('click', (event) => {
+  if (event.target.classList.contains('start')) {
+    document.querySelector('.stop').classList.remove('stop-timer');
+    document.querySelector('.steps-current').innerHTML = 0;
+    mixCubs();
+    timer();
+  }
+  if (event.target.classList.contains('stop')) {
+    document.querySelector('.stop').classList.add('stop-timer');
+  }
+
+});
+
+// Mix Cubs
+function mixCubs() {
+  const cubs = document.querySelectorAll('.cub');
+  const cubsLength = cubs.length;
+  document.querySelector('.container').innerHTML = '';
+  const mixingcubs = [];
+  const mixcounts = createCollectionRandom(0, cubsLength);
+  cubs.forEach((item, index) => {
+    mixingcubs[mixcounts[index]] = item;
+  });
+  console.log(mixingcubs);
+  mixingcubs.forEach((item) => {
+    document.querySelector('.container').append(item);
+  });
+}
+
+// Create random collection for a mixin cubs
+function createCollectionRandom(min, max) {
+  const set = new Set();
+  while (set.size <= max) {
+    const rand = Math.round(min - 0.5 + Math.random() * (max - min + 1));
+    set.add(rand);
+  }
+  let a = [];
+  set.forEach(item => {
+    a.push(item);
+  })
+  return a;
+}
+
+// Event for a change game field size
+const changeSize = document.querySelector('.changeSize');
+changeSize.addEventListener('click', (event) => {
+  if (event.target.tagName === 'BUTTON') {
+    changeSizeOfBoard(event.target.getAttribute('class'));
+    document.querySelector('.size').innerHTML = event.target.innerHTML;
+  }
+});
+
+// function for a change size
+function changeSizeOfBoard(size) {
+  let fieldGame = document.querySelector('.container');
+  fieldGame.innerHTML = '';
+  for (let i = 0; i < fields[size]; i += 1) {
+    const box = document.createElement('div');
+    box.classList = `cub ${'cub' + (i + 1)}`;
+    box.innerText = `${i + 1}`;
+    fieldGame.append(box);
+  }
+  const free = document.createElement('div');
+  free.classList = 'cub free';
+  fieldGame.append(free);
+
+  const quantityColumn = Math.sqrt(fields[size] + 1);
+  fieldGame.style.gridTemplateColumns = `repeat(${quantityColumn}, 1fr)`;
+}
